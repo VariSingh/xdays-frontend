6a9ba7f04e3c81eb894a9308998c98508310fdc2
Varinder Singh <varindersingh271990@gmail.com>
2019-03-31 12:18:30 +0530

    Login screen added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ChallengeComponent } from './challenge/challenge.component';
+import { DayComponent } from './day/day.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {
+  path: 'login',
+  component: LoginComponent,
+},
+{
+  path: 'challenge',
+  component: ChallengeComponent,
+},
+{
+  path: 'day',
+  component: DayComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ChallengeComponent } from './challenge/challenge.component';
+import { DayComponent } from './day/day.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ChallengeComponent,
+    DayComponent
   ],
   imports: [
     BrowserModule,
