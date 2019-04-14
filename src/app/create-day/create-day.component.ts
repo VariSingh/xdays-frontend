af1e3dd730d0560f3b92650c1a70dace5295989c
Varinder Singh <varindersingh271990@gmail.com>
2019-04-14 17:31:12 +0530

    Add progress - disabled dates already having entry

--- src/app/create-day/create-day.component.ts
+++ src/app/create-day/create-day.component.ts
@@ -1,3 +1,4 @@
+import { DayService } from './../services/day.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
@@ -12,40 +13,57 @@ import { IMyDpOptions } from 'mydatepicker';
 })
 export class CreateDayComponent implements OnInit {
 
-  constructor(private formBuilder: FormBuilder, private activeRoute: ActivatedRoute, private challengeService: ChallengeService, private toastr: ToastrService, private router: Router) { }
+  constructor(private formBuilder: FormBuilder, private activeRoute: ActivatedRoute, private challengeService: ChallengeService, public toastr: ToastrService, public router: Router, public route: ActivatedRoute, public dayService: DayService) { }
   dayForm: FormGroup;
   challengeId: string;
-  submit: boolean = false;
+  submit = false;
   d: Date = new Date();
   myDatePickerOptions: IMyDpOptions = {
-    // other options...
     dateFormat: 'dd/mm/yyyy'
   };
 
   ngOnInit() {
-    let ds = new Date();
-    let du = new Date();
+    const ds = new Date();
+    const du = new Date();
 
     ds.setDate(ds.getDate() + 1);
     du.setDate(du.getDate() - 7);
 
     this.myDatePickerOptions.disableSince = { year: ds.getFullYear(), month: ds.getMonth() + 1, day: ds.getDate() }
     this.myDatePickerOptions.disableUntil = { year: du.getFullYear(), month: du.getMonth() + 1, day: du.getDate() }
-    
+    if (localStorage.getItem('disableDays')) {
+      this.myDatePickerOptions.disableDays = JSON.parse(localStorage.getItem('disableDays'));
+    } else {
+      this.router.navigateByUrl('login');
+    }
+
     this.dayForm = this.formBuilder.group({
       description: ['', Validators.required],
       date: [{ date: { year: this.d.getFullYear(), month: this.d.getMonth() + 1, day: this.d.getDate() } }, Validators.required]
     });
-    //this.setDate();
+    // this.setDate();
 
-    //this.dayForm.controls.date.patchValue({ date: '2019-11-11' });
+    // this.dayForm.controls.date.patchValue({ date: '2019-11-11' });
 
     this.challengeId = this.activeRoute.snapshot.params.challengeId;
+    this.getDays();
+  }
+
+  getDays() {
+    this.challengeId = this.route.snapshot.params.challengeId;
+    this.dayService.getDaysOfChallenge(this.challengeId).subscribe((response: any) => {
+      console.log(response);
+      const challengeDetails = response.challengeDetails;
+      const dayList = response.daysList;
+    },
+      (error: any) => {
+
+      });
   }
 
   setDate(): void {
     // Set today date using the patchValue function
-    let date = new Date();
+    const date = new Date();
     this.dayForm.patchValue({
       date: {
         date: {
@@ -62,6 +80,14 @@ export class CreateDayComponent implements OnInit {
     this.dayForm.patchValue({ myDate: null });
   }
 
+  reformatDate(date: any) {
+    const d: Date = new Date();
+    d.setDate(date.date.day);
+    d.setMonth(date.date.month - 1);
+    d.setFullYear(date.date.year);
+    return d;
+  }
+
   addDay() {
     this.submit = true;
     console.log(this.dayForm);
@@ -69,10 +95,11 @@ export class CreateDayComponent implements OnInit {
       return;
     }
 
-    let data = {
+
+    const data = {
       description: this.dayForm.controls.description.value,
-      date: this.dayForm.controls.date.value
-    }
+      date: this.reformatDate(this.dayForm.controls.date.value)
+    };
 
     this.challengeService.addDay(data).subscribe(
       (res) => {
@@ -80,13 +107,13 @@ export class CreateDayComponent implements OnInit {
         this.toastr.success('', 'Progress added');
         setTimeout(() => {
 
-          this.router.navigateByUrl(`/challenge/${this.challengeId}`)
+          this.router.navigateByUrl(`/challenge/${this.challengeId}`);
         }, 2000);
       },
       (error) => {
         console.log(error);
-        this.toastr.error(error, 'Oops! Something went wrong');
-      })
+        this.toastr.error(error.error.message, 'Error');
+      });
   }
 
 }
--- src/app/day/day.component.ts
+++ src/app/day/day.component.ts
@@ -18,7 +18,7 @@ export class DayComponent implements OnInit {
   }
 
   dateDiff(date1: any, date2: any) {
-    //find difference in days between date1 and date2
+    // find difference in days between date1 and date2
     const d1 = new Date(date1);
     const d2 = new Date(date2);
     const diffTime = Math.abs(d2.getTime() - d1.getTime());
@@ -26,77 +26,42 @@ export class DayComponent implements OnInit {
     return diffDays;
   }
 
-  subtractDate(date: Date, step: number) {
+  subtractDate(date: Date, n: number) {
+    // Subtract n days from date 
     const d = new Date(date);
     const currentDate = d.getDate();
-    d.setDate(currentDate - 1);
+    d.setDate(currentDate - n);
     return d;
   }
 
   matchDate(date: Date, daysList: any[]) {
-    //console.log(date);
-    //console.log(daysList);
-    for(let i=0;i<daysList.length;i++){
-      let diff = this.dateDiff(daysList[i].date,date);
-      //console.log(daysList[i].date+" "+date+" "+diff);
-      if(diff==1){//day matched. Make it zero later
-          return daysList[i];
+    // console.log(date);
+    // console.log(daysList);
+    for (let i = 0; i < daysList.length; i++) {
+      const diff = this.dateDiff(daysList[i].date, date);
+      if (diff === 1) {// day matched. Make it zero later
+        return daysList[i];
         // break;
       }
     }
   }
 
   getDays() {
-    this.challengeId = this.route.snapshot.params['challengeId'];
+    this.challengeId = this.route.snapshot.params.challengeId;
     this.dayService.getDaysOfChallenge(this.challengeId).subscribe((response: any) => {
       console.log(response);
       const challengeDetails = response.challengeDetails;
-     // this.totalDays = new Array(response.challengeDetails.number_of_days);
+      // this.totalDays = new Array(response.challengeDetails.number_of_days);
       this.daysList = response.daysList;
-
-
-      //const startDate = new Date(challengeDetails.start_date);
-      //const today = new Date();
-      //find difference in days
-    //  console.log(this.dateDiff(startDate, today));
-
-      //create an array starting from 7 days behind today till today;
-      //let date = new Date();
-      //let arr = [];
-     // for (let i = 0; i < 7; i++) {
-
-        //search if there is any entry for this day; if yes then use it instead of obj
-       // console.log(date);
-       // let match = this.matchDate(date, this.daysList);
-       // let obj :any;
-       // if(match){
-       //   obj = match;
-       // }else{
-       //   obj = {
-       //     challenge: challengeDetails._id,
-       //     date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}T00:00:00.000Z`,
-       //     description: ''
-      //    }
-     //   }
-       // arr.push(obj);
-       // date = this.subtractDate(date, 1);
-     // }
-     // console.log(arr);
-
-      //todo: if start date is greater than today then show total Days
-      //else
-      //don't show anything
-
-      //in total days show data in those days which are filled.
-      //-----------------------------------
-      //if start date is lesser than today then show count(today-startdate) days list
-      //if count(today-startdate) is greater than 7 then calculate 7 days only.
-      //blank days will only be shown for past 7 days . Before that show days as it is.
-
+      const disableDaysList = this.daysList.map((item: any) => {
+        const d = new Date(item.date);
+        return {year: d.getFullYear(), month: d.getMonth() + 1, day: d.getDate()};
+      });
+      localStorage.setItem('disableDays', JSON.stringify(disableDaysList));
     },
       (error: any) => {
 
-      })
+      });
   }
 
 }
